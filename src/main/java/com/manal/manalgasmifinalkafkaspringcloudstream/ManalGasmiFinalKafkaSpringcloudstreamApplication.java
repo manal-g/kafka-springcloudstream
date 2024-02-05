f0af3ac454b67fb780367beb6648b5d2f17b6524
Your Name <you@example.com>
2024-02-05 19:15:33 +0100

    fc

---
+++ src/main/java/com/manal/manalgasmifinalkafkaspringcloudstream/ManalGasmiFinalKafkaSpringcloudstreamApplication.java
@@ -0,0 +1,13 @@
+package com.manal.manalgasmifinalkafkaspringcloudstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManalGasmiFinalKafkaSpringcloudstreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManalGasmiFinalKafkaSpringcloudstreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manal/manalgasmifinalkafkaspringcloudstream/ManalGasmiFinalKafkaSpringcloudstreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manal.manalgasmifinalkafkaspringcloudstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManalGasmiFinalKafkaSpringcloudstreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
